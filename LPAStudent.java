8e1a0d6f7d7d1202a2e98214999d28f6dece2365
Himanshu Gadge <52168989+gadgehimanshu7@users.noreply.github.com>
2023-02-16 22:25:14 +0530

    Delete LPAStudent.java

--- LPAStudent.java
+++
@@ -1,5 +0,0 @@
-public record LPAStudent(String id, String name, String dob, String classList)  //part in '()'paranthesis is called record header.
-
-    //doesnt include '' when printing output.
-{
-}
